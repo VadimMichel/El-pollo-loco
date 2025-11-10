47ecbfa3944c8dd9ff2dbe8147b4154b96eaa0d6
VadimMichel <vadimmichel5@gmail.com>
2025-11-10 06:53:25 +0100

    refactor: clean up Cloud class

--- scripts/models/cloud.class.js
+++ scripts/models/cloud.class.js
@@ -6,19 +6,27 @@ class Cloud extends MovableObject{
     speed;
 
     constructor (world){
-        super()
-        this.world = world
-        if(this.randomZerroOrOne() == 1){
-            this.loadImage("img/5_background/layers/4_clouds/1.png");
-        }else{
-            this.loadImage("img/5_background/layers/4_clouds/2.png")
-        }
-        this.x = Math.random() * 2500;
-        this.speed = 0.15+ Math.random() * 1
+        super();
+        this.setVariant();
+        this.setPositionAndSpeed(world);
         this.animate();
     }
 
     animate(){
         setStoppableInterval(() => this.moveLeft(), 50);
+    }
+
+    setPositionAndSpeed(world){
+        this.world = world;
+        this.x = Math.random() * 2500;
+        this.speed = 0.15+ Math.random() * 1;
+    }
+
+    setVariant(){
+        if(this.randomZerroOrOne() == 1){
+            this.loadImage("img/5_background/layers/4_clouds/1.png");
+        }else{
+            this.loadImage("img/5_background/layers/4_clouds/2.png");
+        }
     }
 }
