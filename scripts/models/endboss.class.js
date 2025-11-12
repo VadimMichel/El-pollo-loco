50458707a385da61dc8b4946ecb9cc39404a3eea
VadimMichel <vadimmichel5@gmail.com>
2025-11-12 09:59:42 +0100

    docs: add JSDoc to endboss class

--- scripts/models/endboss.class.js
+++ scripts/models/endboss.class.js
@@ -1,11 +1,60 @@
+/**
+ * @fileoverview Endboss enemy class. Extends MovableObject and implements
+ * complex behaviour for the level boss including staged entrance, alert,
+ * walking, attacking, hurt and death animations.
+ *
+ * The endboss uses several image sets for different states and controls when
+ * to start the boss fight via `startBossFight` and `startAnimation`.
+ *
+ * @extends {MovableObject}
+ */
 class Endboss extends MovableObject{
+    /**
+     * Sprite width in pixels.
+     * @type {number}
+     */
     width = 200;
+
+    /**
+     * Sprite height in pixels.
+     * @type {number}
+     */
     height = 400;
+
+    /**
+     * Vertical position in world coordinates.
+     * @type {number}
+     */
     y = 45;
+
+    /**
+     * Damage value applied when boss is hit.
+     * @type {number}
+     */
     recievedDamage = 20;
+
+    /**
+     * Flag that triggers the boss fight behaviour when set to true.
+     * @type {boolean}
+     */
     startBossFight = false;
+
+    /**
+     * Counter used to control staged animations (alert -> walk -> attack).
+     * @type {number}
+     */
     startAnimation = 0;
+
+    /**
+     * Horizontal movement speed.
+     * @type {number}
+     */
     speed = 3;
+
+    /**
+     * Collision box offsets relative to sprite bounds.
+     * @type {{top:number,left:number,right:number,bottom:number}}
+     */
     offset = {
         top: 100,
         left: 30,
@@ -13,6 +62,10 @@ class Endboss extends MovableObject{
         bottom: 15
     };
 
+    /**
+     * Walking animation frames.
+     * @type {string[]}
+     */
     IMAGES_WALKING = [
         "img/4_enemie_boss_chicken/1_walk/G1.png",
         "img/4_enemie_boss_chicken/1_walk/G2.png",
@@ -20,6 +73,10 @@ class Endboss extends MovableObject{
         "img/4_enemie_boss_chicken/1_walk/G4.png"
     ];
 
+    /**
+     * Alert animation frames (boss entrance / pre-attack).
+     * @type {string[]}
+     */
     IMAGES_ALERT = [
         "img/4_enemie_boss_chicken/2_alert/G5.png",
         "img/4_enemie_boss_chicken/2_alert/G6.png",
@@ -31,6 +88,10 @@ class Endboss extends MovableObject{
         "img/4_enemie_boss_chicken/2_alert/G12.png",
     ];
 
+    /**
+     * Attack animation frames.
+     * @type {string[]}
+     */
     IMAGES_ATTACK = [
         "img/4_enemie_boss_chicken/3_attack/G13.png",
         "img/4_enemie_boss_chicken/3_attack/G14.png",
@@ -42,18 +103,31 @@ class Endboss extends MovableObject{
         "img/4_enemie_boss_chicken/3_attack/G13.png"
     ];
 
+    /**
+     * Hurt animation frames.
+     * @type {string[]}
+     */
     IMAGES_HURT = [
         "img/4_enemie_boss_chicken/4_hurt/G21.png",
         "img/4_enemie_boss_chicken/4_hurt/G22.png",
         "img/4_enemie_boss_chicken/4_hurt/G23.png"
     ];
 
+    /**
+     * Dead animation frames.
+     * @type {string[]}
+     */
     IMAGES_DEAD = [
         "img/4_enemie_boss_chicken/5_dead/G24.png",
         "img/4_enemie_boss_chicken/5_dead/G25.png",
         "img/4_enemie_boss_chicken/5_dead/G26.png"
     ];
 
+    /**
+     * Construct the Endboss, preload images, set start position and begin animation loops.
+     *
+     * @returns {void}
+     */
     constructor(){
         super();
         this.loadAllImages();
@@ -61,17 +135,35 @@ class Endboss extends MovableObject{
         this.animate();
     }
 
+    /**
+     * Start repeating intervals for boss animation and movement.
+     * Uses global setStoppableInterval so intervals can be cleared by the game.
+     *
+     * @returns {void}
+     */
     animate(){
         setStoppableInterval(() => this.animateEndboss(), 200);
         setStoppableInterval(() => this.makeBoosMoveLeft(), 100);
     }
 
+    /**
+     * Move the boss left under specified conditions:
+     * - When fight started and boss is still beyond a threshold (entrance)
+     * - Or when alert animation finished and the boss isn't dead.
+     *
+     * @returns {void}
+     */
     makeBoosMoveLeft(){
         if((this.startBossFight && this.x > 2500 && !this.isDead()) || (this.startAnimation >= this.IMAGES_WALKING.length +1 && !this.isDead())){
             this.moveLeft(this.speed);
         }
     }
 
+    /**
+     * Decide which animation to run based on current state (dead, hurt, walk, alert, attack).
+     *
+     * @returns {void}
+     */
     animateEndboss(){
         if(this.isDead()){
             this.animateDead();
@@ -86,40 +178,82 @@ class Endboss extends MovableObject{
         }  
     }
 
+    /**
+     * Whether alert animation should run.
+     * @returns {boolean}
+     */
     shouldAlert(){
         return this.startBossFight && this.x > 2490 && this.startAnimation < this.IMAGES_ALERT.length +1;
     }
 
+    /**
+     * Whether walking animation should run.
+     * @returns {boolean}
+     */
     shouldWalk(){
         return this.startBossFight && this.x > 2500;
     }
 
+    /**
+     * Whether attack animation should run (after walking/alert counters).
+     * @returns {boolean}
+     */
     shouldAttack(){
         return this.startAnimation >= this.IMAGES_WALKING.length +1;
     }
 
+    /**
+     * Play attack frames (resets non-looping counter).
+     *
+     * @returns {void}
+     */
     animateAttack(){
         this.j = 0;
         this.animateImage(this.IMAGES_ATTACK);
     }
 
+    /**
+     * Play walking frames.
+     *
+     * @returns {void}
+     */
     animateWalking(){
         this.animateImage(this.IMAGES_WALKING);
     }
 
+    /**
+     * Play alert frames and advance the startAnimation counter.
+     *
+     * @returns {void}
+     */
     animateAlert(){
         this.animateImage(this.IMAGES_ALERT);
-        this.startAnimation++
+        this.startAnimation++;
     }
 
+    /**
+     * Play hurt frames.
+     *
+     * @returns {void}
+     */
     animateHurt(){
         this.animateImage(this.IMAGES_HURT);
     }
 
+    /**
+     * Play dead frames.
+     *
+     * @returns {void}
+     */
     animateDead(){
         this.animateImage(this.IMAGES_DEAD);
     }
 
+    /**
+     * Preload representative image and all image sets used by the boss.
+     *
+     * @returns {void}
+     */
     loadAllImages(){
         this.loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
@@ -129,6 +263,11 @@ class Endboss extends MovableObject{
         this.loadImages(this.IMAGES_ATTACK);
     }
 
+    /**
+     * Set the initial horizontal position for the boss (off-screen start).
+     *
+     * @returns {void}
+     */
     setPosition(){
         this.x = 2800;
     }
