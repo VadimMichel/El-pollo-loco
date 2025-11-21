d0b61f020afef62bfb3afae2679b020b184326ef
VadimMichel <vadimmichel5@gmail.com>
2025-11-21 10:26:43 +0100

    fix: remove console space bug

--- scripts/models/background-objects.class.js
+++ scripts/models/background-objects.class.js
@@ -11,54 +11,38 @@
  * @extends {MovableObject}
  */
 class BackgroundObject extends MovableObject {
-    /**
-     * Width of the background image in pixels.
-     * @type {number}
-     */
-    width = 720;
+	/**
+	 * Width of the background image in pixels.
+	 * @type {number}
+	 */
+	width = 720;
 
-    /**
-     * Height of the background image in pixels.
-     * @type {number}
-     */
-    height = 480;
+	/**
+	 * Height of the background image in pixels.
+	 * @type {number}
+	 */
+	height = 480;
 
-    /**
-     * Create a BackgroundObject, load its image and set its horizontal position.
-     *
-     * @param {string} imagePath - Path to the background image file.
-     * @param {number} x - Initial x position in pixels.
-     */
-    constructor(imagePath, x) {
-        super().loadImage(imagePath);
-        this.setPosition(x);
-    }
+	/**
+	 * Create a BackgroundObject, load its image and set its horizontal position.
+	 *
+	 * @param {string} imagePath - Path to the background image file.
+	 * @param {number} x - Initial x position in pixels.
+	 */
+	constructor(imagePath, x) {
+		super().loadImage(imagePath);
+		this.setPosition(x);
+	}
 
-    /**
-     * Set the x,y position of the background object. The y position is computed so the bottom
-     * of the background aligns with the canvas floor (assumed canvas height 480).
-     *
-     * @param {number} x - X coordinate in pixels.
-     * @returns {void}
-     */
-    setPosition(x) {
-        this.y = 480 - this.height;
-        this.x = x;
-    }
-
-    /**
-     * Return a nicely formatted debug string as JSON for console output.
-     * Use: console.log(JSON.stringify(bgObj, null, 2))
-     *
-     * @returns {string}
-     */
-    toString(){
-        return JSON.stringify({
-            class: 'BackgroundObject',
-            x: this.x,
-            y: this.y,
-            width: this.width,
-            height: this.height
-        }, null, 2);
-    }
+	/**
+	 * Set the x,y position of the background object. The y position is computed so the bottom
+	 * of the background aligns with the canvas floor (assumed canvas height 480).
+	 *
+	 * @param {number} x - X coordinate in pixels.
+	 * @returns {void}
+	 */
+	setPosition(x) {
+		this.y = 480 - this.height;
+		this.x = x;
+	}
 }
--- scripts/models/level.class.js
+++ scripts/models/level.class.js
@@ -8,57 +8,57 @@
  * @class Level
  */
 class Level {
-    /**
-     * Array of enemy instances present in this level.
-     * @type {Array<MovableObject>|Array<Enemy>}
-     */
-    enemies;
+	/**
+	 * Array of enemy instances present in this level.
+	 * @type {Array<MovableObject>|Array<Enemy>}
+	 */
+	enemies;
 
-    /**
-     * Array of BackgroundObject instances used for parallax background layers.
-     * @type {BackgroundObject[]}
-     */
-    backgroundObjects;
+	/**
+	 * Array of BackgroundObject instances used for parallax background layers.
+	 * @type {BackgroundObject[]}
+	 */
+	backgroundObjects;
 
-    /**
-     * Array of Cloud instances used as decorative background elements.
-     * @type {Cloud[]}
-     */
-    clouds;
+	/**
+	 * Array of Cloud instances used as decorative background elements.
+	 * @type {Cloud[]}
+	 */
+	clouds;
 
-    /**
-     * Array of CollectableObject instances representing coins.
-     * @type {CollectableObject[]}
-     */
-    coins;
+	/**
+	 * Array of CollectableObject instances representing coins.
+	 * @type {CollectableObject[]}
+	 */
+	coins;
 
-    /**
-     * Array of CollectableObject instances representing bottles.
-     * @type {CollectableObject[]}
-     */
-    bottle;
+	/**
+	 * Array of CollectableObject instances representing bottles.
+	 * @type {CollectableObject[]}
+	 */
+	bottle;
 
-    /**
-     * X coordinate (in world pixels) that defines the level's end boundary.
-     * Used to limit player movement and to trigger end-of-level behaviour.
-     * @type {number}
-     */
-    level_end_x = 2157;
+	/**
+	 * X coordinate (in world pixels) that defines the level's end boundary.
+	 * Used to limit player movement and to trigger end-of-level behaviour.
+	 * @type {number}
+	 */
+	level_end_x = 2157;
 
-    /**
-     * Create a Level instance.
-     *
-     * @param {Array<MovableObject>|Array<Enemy>} enemies - Enemies to populate the level.
-     * @param {Cloud[]} clouds - Clouds used in the level background.
-     * @param {BackgroundObject[]} backgroundObjects - Parallax background layers.
-     * @param {CollectableObject[]} coins - Collectable coins.
-     * @param {CollectableObject[]} bottle - Collectable bottles.
-     */
-    constructor(enemies, clouds, backgroundObjects, coins, bottle){
-        this.enemies = enemies;
-        this.clouds = clouds;
-        this.backgroundObjects = backgroundObjects;
-        this.coins = coins;
-        this.bottle = bottle;
-    }
+	/**
+	 * Create a Level instance.
+	 *
+	 * @param {Array<MovableObject>|Array<Enemy>} enemies - Enemies to populate the level.
+	 * @param {Cloud[]} clouds - Clouds used in the level background.
+	 * @param {BackgroundObject[]} backgroundObjects - Parallax background layers.
+	 * @param {CollectableObject[]} coins - Collectable coins.
+	 * @param {CollectableObject[]} bottle - Collectable bottles.
+	 */
+	constructor(enemies, clouds, backgroundObjects, coins, bottle){
+		this.enemies = enemies;
+		this.clouds = clouds;
+		this.backgroundObjects = backgroundObjects;
+		this.coins = coins;
+		this.bottle = bottle;
+	}
 }
